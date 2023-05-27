e32a6ebc3cc18232b66e63809bb68c5a9ddbdbee
Gael Melo <gaelmmelo@gmail.com>
2023-05-27 18:56:18 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from spotipy.oauth2 import SpotifyOAuth
+
+scope="user-library-modify"
+
+render = render_template
+app = Flask(__name__)
+
+@app.route("/")
+def landing_page():
+    return render('index.html')
