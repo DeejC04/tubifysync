ad98003ff4dddd3492848be951149b3dafa6291a
Deej Cabral <deejc04@Deejs-MacBook-Air.local>
2023-06-05 11:33:50 -0400

    added reworked environment variables

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def get_spotify_key():
 
 # Runs Flask app when the Python script is opened directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="http://localhost:5000/")
